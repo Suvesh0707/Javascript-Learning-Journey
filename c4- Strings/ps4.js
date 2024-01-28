1d29ba53d857367e83e27414f6373ae78911e8ff
Suvesh Pagam <156836161+Suvesh0707@users.noreply.github.com>
2024-01-28 16:10:46 +0530

    Add files via upload

---
+++ c4- Strings/ps4.js
@@ -0,0 +1,18 @@
+// Practice Set 4
+
+// Question 1 - What will the following print in javascript ! -> console.log("har\"".length)
+a = "har\""
+console.log(a.length) // -> 4
+
+// Question 2 - Write a program to convert a string to lowercase.
+b = "SUVEsh"
+console.log(b.toLowerCase())
+
+// Question 3 - Extract the amount of this string. "please give RS 1000"
+let str1 = "please give RS 1000"
+let amount = console.log(str1.slice(12))
+
+// Question 4 - Try to change 4th character of a string would you able to do it ?
+let str2 = "rubina"
+friend[3]= "r"
+console.log(friend) // friend is not changed, because string is immutable
